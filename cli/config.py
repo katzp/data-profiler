acab60e562acc0232dc92daf0ccac03ee793ac2b
Peter Katz <pkatz@vistaprint.com>
2021-10-27 15:54:23 -0400

    cli changes

--- cli/config.py
+++ cli/config.py
@@ -2,14 +2,6 @@ CONFIG = """
 {
     "new_cluster": {
         "spark_version": "7.6.x-scala2.12",
-        "aws_attributes": {
-            "availability": "SPOT_WITH_FALLBACK",
-            "first_on_demand": 1,
-            "zone_id": "eu-west-1c",
-            "ebs_volume_type": "GENERAL_PURPOSE_SSD",
-            "ebs_volume_size": 100,
-            "ebs_volume_count": 3
-        },
         "node_type_id": "c4.4xlarge",
         "driver_node_type_id": "r4.4xlarge",
         "autoscale": {
@@ -33,6 +25,6 @@ CONFIG = """
         }
     ],
     "spark_jar_task": {
-        "main_class_name": "com.vistaprint.data.profiler.Pipeline"
+        "main_class_name": "com.katzp.data.profiler.Pipeline"
     }
 }"""
--- cli/profiler.py
+++ cli/profiler.py
@@ -3,7 +3,7 @@ import os
 from typing import Tuple
 import click
 import requests
-from profiler.cli.config import CONFIG
+from cli.config import CONFIG
 
 
 @click.group()
@@ -28,11 +28,32 @@ def cli():
     "--where",
     help="""
     [Optional] Snowflake where predicate to filter rows to profile. For example --where "WEBSITE_LOCALE = 'US'"
-    """
+    """,
 )
-@click.option("--tag", help="[Optional] Tag to apply to profiling job. For helping to filter in Looker.")
-@click.option("--database", hidden=True)
-def profile(table: str, column: Tuple[str], where: str, tag: str, database: str):
+@click.option(
+    "--tag",
+    help="[Optional] Tag to apply to profiling job. For helping to filter in Looker.",
+)
+@click.option("--database", required=True, help="Snowflake database")
+@click.option("--schema", required=True, help="Snowflake schema")
+@click.option("--user", required=True, help="Snowflake user")
+@click.option("--password", required=True, help="Snowflake password")
+@click.option("--role", required=True, help="Snowflake role")
+@click.option("--warehouse", required=True, help="Snowflake warehouse")
+@click.option("--host", required=True, help="Databricks host name")
+def profile(
+    table: str,
+    column: Tuple[str],
+    where: str,
+    tag: str,
+    database: str,
+    schema: str,
+    user: str,
+    password: str,
+    role: str,
+    warehouse: str,
+    host: str,
+):
     token = os.getenv("DNA_PROFILER_DATABRICKS_TOKEN")
     if not token:
         raise Exception(
@@ -48,19 +69,33 @@ def profile(table: str, column: Tuple[str], where: str, tag: str, database: str)
         jar_params.extend(["--where", where.upper().replace("WHERE", "")])
     if tag:
         jar_params.extend(["--tag", tag])
-    if database:
-        jar_params.extend(["--database", database])
-    payload["spark_jar_task"]["parameters"] = jar_params
-    endpoint = (
-        "https://dbc-dna-common-prd.cloud.databricks.com" + "/api/2.0/jobs/runs/submit"
+    jar_params.extend(
+        [
+            "--database",
+            database,
+            "--schema",
+            schema,
+            "--user",
+            user,
+            "--password",
+            password,
+            "--role",
+            role,
+            "--warehouse",
+            warehouse,
+        ]
     )
+    payload["spark_jar_task"]["parameters"] = jar_params
+    endpoint = host + "/api/2.0/jobs/runs/submit"
     response = requests.post(
         endpoint,
         headers={"Authorization": f"Bearer {token}"},
         json=payload,
     )
     if response.ok:
-        click.echo("Submitted profiling job. Check Looker report in 5 minutes\nhttps://cimpress.eu.looker.com/dashboards-next/7961")
+        click.echo(
+            "Submitted profiling job. Check Looker report in 5 minutes\nhttps://cimpress.eu.looker.com/dashboards-next/7961"
+        )
     else:
         click.echo(f"ERROR in submitting profiler job: {response.json()}")
 
